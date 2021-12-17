c03b3224ac905cd84700a1522d1abb5f22086262
Jathurchan Selvakumar <mail@jathurchan.com>
2021-12-17 08:34:26 +0100

    Display welcome message

---
+++ enseah1.c
@@ -0,0 +1,19 @@
+
+// Display a welcome message, followed by a simple prompt
+
+#include <string.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 128
+
+int main (int argc, char *argv[]) {
+	
+	char message[] = "Welcome to ENSEA Tiny Shell.\nTo quit, write 'exit'.\n";
+	char beg[BUFF_SIZE] = "enseash ";
+
+	write(STDOUT_FILENO, message, strlen(message));
+	write(STDOUT_FILENO, beg, strlen(beg));
+	
+	
+	return 0;
+}
