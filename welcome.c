88e7a1c26555e245de3544e3550a9de71201d890
Uyen-My <uyen-my.nguyen@ensea.fr>
2021-12-17 02:14:56 -0500

    Welcoming prompt modification

--- welcome.c
+++ welcome.c
@@ -3,7 +3,7 @@
 
 int main (int argc, char *argv[]) {
 	char message[200] = "Welcome to ENSEA Tiny Shell.\nTo quit, tap 'exit'.\nenseash % \n";
-	#include <unistd.h>
+
 	write(STDOUT_FILENO, message, strlen(message));
 	
 	return 0;
